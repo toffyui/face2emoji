2a1ca0740541e9acde0d8bdcc449b46aece46879
yuikoito <yuiko.dev@gmail.com>
2021-08-09 01:48:56 +0900

    fix::bug: update nextcofig.json

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
+
+    return config;
+  },
 };
