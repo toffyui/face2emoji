8d04f179356ebb3545bdbb0720ea82e89ca5df28
yuikoito <yuiko.dev@gmail.com>
2021-08-09 00:58:45 +0900

    feat::tada: add face expressions detection

---
+++ components/Loader.tsx
@@ -0,0 +1,10 @@
+import styles from "../styles/Loader.module.css";
+const Loader = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.loader5}></div>
+    </div>
+  );
+};
+
+export default Loader;
